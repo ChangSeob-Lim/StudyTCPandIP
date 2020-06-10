f9e8e22d9c31b8f6329eebad96b420e5c46b7520
ChangSeob-Lim <limchangsz@gmail.com>
2020-06-10 16:31:26 +0900

    최초 커밋

---
+++ main_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	
+	printf("인자갯수 : %d\n", argc);
+	printf("프로그램경로 : %s\n", argv[0]);
+
+	for(int i=1; i<argc; i++)
+	{
+		printf("추가된 인자 : %s\n", argv[i]);
+	}
+
+	return 0;
+}  
+
